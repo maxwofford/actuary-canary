7e2d6fc4ea0fee4f82a6b8bb1900eaf99e36fb1b
Max Wofford <max@maxwofford.com>
2016-09-26 18:59:27 -0400

    Create db:seed task for Stocks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+nasdaq = NasdaqAdapter.new
+gfinance = GoogleFinanceAdapter.new
+
+# Populate stock symbols
+symbols = nasdaq.get_all_stock_symbols
+symbols.each do |symbol|
+  gid = gfinance.get_gid_by_symbol symbol
+  share_price = gfinance.get_price_by_gid gid
+
+  Stock.create(symbol: symbol, gid: gid, share_price: share_price)
+end
