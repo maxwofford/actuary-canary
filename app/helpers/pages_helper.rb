514b570b2fbbc423b9e34302b737cdf16c250c62
Max Wofford <max@maxwofford.com>
2016-11-17 21:36:41 -0500

    Add home page

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,10 @@
+module PagesHelper
+  def full_title page_title
+    base_title = 'Actuary Canary'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+end
